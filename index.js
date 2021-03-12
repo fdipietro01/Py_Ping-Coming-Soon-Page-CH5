cda7ce480482ab7dd78b8ab9b975d959ed3fd554
Flavio <fdipietro01@gmail.com>
2021-03-12 04:39:15 -0300

    validador de formulario version mobile

---
+++ index.js
@@ -0,0 +1,32 @@
+const but = document.getElementById("but");
+but.addEventListener("click",
+    function(e) {
+        const form = document.getElementById("form");
+        const inp = document.getElementById("input");
+        const fm = document.getElementById("fm");
+        const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+        if (inp.value == "" || inp.value == "null" || emailRegex.test(inp.value) == false) {
+            inp.setAttribute("class", "input-error");
+            inp.setAttribute("placeholder", "example@gmail.com");
+            inp.value = "";
+            fm.setAttribute("class", "form-container-error");
+
+            /*comprobar si ya salio el error anteriormente o es la primera vez*/
+            const fail = document.getElementById("fail");
+            if (fail != null) {
+                form.removeChild(fail);
+            }
+            /*crea aviso de error e insertarlo en dom*/
+            const error = document.createElement("p");
+            error.textContent = "Please provide a valid email address";
+            error.setAttribute("class", "p-error");
+            error.setAttribute("id", "fail")
+            frag = document.createDocumentFragment();
+            frag.append(error);
+            form.insertBefore(error, but);
+
+        } else {
+            form.submit();
+        }
+    })
