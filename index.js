32f80af62e5675a9af61596c6e5b0db1a0b2da7f
Flavio <fdipietro01@gmail.com>
2021-03-15 11:56:58 -0300

    resposive done

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function validar() {
-    const form = document.getElementById("form");
+    const form= document.getElementById("form");
+    const inpContainer = document.getElementById("input-container");
     const inp = document.getElementById("input");
     const fm = document.getElementById("fm");
     const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
     if (inp.value == "" || inp.value == "null" || emailRegex.test(inp.value) == false) {
         inp.setAttribute("class", "input-error");
-        inp.setAttribute("placeholder", "example@gmail.com");
+        inp.setAttribute("placeholder", "example@email.com");
         inp.value = "";
         fm.setAttribute("class", "form-container-error");
 
         /*comprobar si ya salio el error anteriormente o es la primera vez*/
         const fail = document.getElementById("fail");
         if (fail != null) {
-            form.removeChild(fail);
+            inpContainer.removeChild(fail);
         }
         /*crea aviso de error e insertarlo en dom*/
         const error = document.createElement("p");
@@ -22,7 +23,7 @@ function validar() {
         error.setAttribute("id", "fail")
         frag = document.createDocumentFragment();
         frag.append(error);
-        form.insertBefore(error, but);
+        inpContainer.append(frag);
 
     } else {
         form.submit();
